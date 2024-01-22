683e72b9150cd8480fb882ac69bbe6a514961af2
Badara <badou017@gmail.com>
2024-01-22 21:33:22 +0000

    Styling Interface

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -10,10 +10,16 @@ export default class extends Controller {
   }
 
   search() {
-    console.log('fetching');
-    fetch(`/search?query=${encodeURIComponent(this.inputTarget.value)}`)
-        .then(response => response.text())
-        .then(html => this.resultsTarget.innerHTML = html)
-        .catch(error => console.error(error));
+    if (this.inputTarget.value.trim() === "") {
+      this.resultsTarget.innerHTML = `
+      <div class="d-flex align-items-center justify-content-center" style="height:200px;">
+        <p class="text-muted">Search for prompt</p>
+      </div>`;
+    } else {
+      fetch(`/search?query=${encodeURIComponent(this.inputTarget.value)}`)
+          .then(response => response.text())
+          .then(html => this.resultsTarget.innerHTML = html)
+          .catch(error => console.error(error));
+    }
   }
 }
